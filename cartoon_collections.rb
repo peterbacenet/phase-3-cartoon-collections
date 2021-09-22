d09011ee9234450e631fad928cb128cf4466ef1f
Peter Bacenet <peterbacenet@icloud.com>
2021-09-22 12:59:52 -0400

    commit tree 5c0072ed11de49cca031867ab31a2ac6bc315534

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+names.map.with_index(1) do |dwarves, i|
+   puts "#{i}. #{dwarves}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |elements|
+    "#{elements.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any ? {do |call|
+    call.size > 4
+  }
 end
+  # calls.each do |call|
+  #   return false if call.size > 4
+  #   else true
+  # end
+end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
